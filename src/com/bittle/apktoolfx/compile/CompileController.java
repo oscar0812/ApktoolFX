f8a9b86d87f1e697cf41ddf16b045177b9b8e20a
oscar0812 <oscar0812torres@gmail.com>
2017-10-14 01:37:38 -0500

    clean spacing

--- src/com/bittle/apktoolfx/compile/CompileController.java
+++ src/com/bittle/apktoolfx/compile/CompileController.java
@@ -17,7 +17,7 @@ import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class CompileController implements Initializable{
+public class CompileController implements Initializable {
     @FXML
     TextField compile_folder_text_field;
 
@@ -36,6 +36,7 @@ public class CompileController implements Initializable{
     @FXML
     Text output_dir_text;
     private String current_folder_path;
+
     public void initialize(URL url, ResourceBundle bundle) {
         log_scroll_pane.setFitToWidth(true);
     }
@@ -67,7 +68,7 @@ public class CompileController implements Initializable{
         }
     }
 
-    public void compile_click(){
+    public void compile_click() {
         LOGGER.setGUIVars(log_text_area, log_scroll_pane);
         // d -f /Users/oscartorres/Downloads/telegram.apk -o /Users/oscartorres/Desktop/
 
@@ -81,15 +82,15 @@ public class CompileController implements Initializable{
             protected Void call() throws Exception {
                 // include any options
                 String params = "b ";
-                if(force_checkbox.isSelected())
-                    params+=" -f ";
+                if (force_checkbox.isSelected())
+                    params += " -f ";
 
                 String[] p = params.split("\\s+");
                 p = append(p, current_folder_path);
 
                 // add the output directory to the end
-                if(!output_dir_text.getText().isEmpty() &&
-                        !output_dir_text.getText().toLowerCase().equals("no directory selected")){
+                if (!output_dir_text.getText().isEmpty() &&
+                        !output_dir_text.getText().toLowerCase().equals("no directory selected")) {
 
                     p = append(p, "-o");
                     p = append(p, output_dir_text.getText());
@@ -108,9 +109,9 @@ public class CompileController implements Initializable{
 
     }
 
-    private String[] append(String[] arr, String a){
-        String[] temp = new String[arr.length+1];
-        for(int x = 0; x<arr.length; x++){
+    private String[] append(String[] arr, String a) {
+        String[] temp = new String[arr.length + 1];
+        for (int x = 0; x < arr.length; x++) {
             temp[x] = arr[x];
         }
         temp[arr.length] = a;
@@ -118,7 +119,7 @@ public class CompileController implements Initializable{
         return temp;
     }
 
-    public void change_output_dir_click(){
+    public void change_output_dir_click() {
         Alert alert = new Alert(Alert.AlertType.WARNING);
         alert.setTitle("Warning");
         alert.setContentText("All chosen folder contents will be erased!");
