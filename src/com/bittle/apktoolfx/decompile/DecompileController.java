9591df4c2581817d16b9d6d68896021c0580752b
oscar0812 <oscar0812torres@gmail.com>
2017-10-14 01:34:16 -0500

    checkbox logic and output dir logic

--- src/com/bittle/apktoolfx/decompile/DecompileController.java
+++ src/com/bittle/apktoolfx/decompile/DecompileController.java
@@ -4,10 +4,7 @@ import com.bittle.apktoolfx.LOGGER;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
@@ -35,15 +32,17 @@ public class DecompileController implements Initializable{
     @FXML
     Text output_dir_text;
 
+    // checkbox settings
+    @FXML CheckBox force_checkbox;
+    @FXML CheckBox no_res_checkbox;
+    @FXML CheckBox no_src_checkbox;
+
     // apk vars
-    String current_apk_path = "";
+    private String current_apk_path = "";
 
     //@Override
     public void initialize(URL url, ResourceBundle bundle) {
-        // needed to tell the logger class where to log to
-        LOGGER.setGUIVars(log_text_area, log_scroll_pane);
         log_scroll_pane.setFitToWidth(true);
-        LOGGER.getInstance().NORMAL("Started up successfully.");
     }
 
     @FXML
@@ -64,16 +63,18 @@ public class DecompileController implements Initializable{
                 // apk file dragged in
                 current_apk_path = file.getAbsolutePath();
                 apk_text_field.setText(file.getName());
+
+                LOGGER.setGUIVars(log_text_area, log_scroll_pane);
                 LOGGER.getInstance().FINE("File dropped: " + file.getAbsolutePath());
             }
         } catch (Exception e) {
-            System.out.println(e.toString());
+            LOGGER.getInstance().ERROR(e.toString());
         }
     }
 
     @FXML
     void decompile_click() {
-
+        LOGGER.setGUIVars(log_text_area, log_scroll_pane);
         // d -f /Users/oscartorres/Downloads/telegram.apk -o /Users/oscartorres/Desktop/
 
         if (apk_text_field.getText().trim().isEmpty()) {
@@ -84,7 +85,27 @@ public class DecompileController implements Initializable{
         Task task = new Task<Void>() {
             @Override
             protected Void call() throws Exception {
-                brut.apktool.Main.main(new String[]{"d", current_apk_path});
+                // include any options
+                String params = "d ";
+                if(force_checkbox.isSelected())
+                    params+=" -f ";
+                if(no_res_checkbox.isSelected())
+                    params+=" -r ";
+                if(no_src_checkbox.isSelected())
+                    params+=" -s ";
+                String[] p = params.split("\\s+");
+                p = append(p, current_apk_path);
+
+                // add the output directory to the end
+                if(!output_dir_text.getText().isEmpty() &&
+                        !output_dir_text.getText().toLowerCase().equals("no directory selected")){
+
+                    p = append(p, "-o");
+                    p = append(p, output_dir_text.getText());
+
+                }
+
+                brut.apktool.Main.main(p);
                 LOGGER.getInstance().NORMAL("-----------------------------------------------");
                 return null;
             }
@@ -93,7 +114,17 @@ public class DecompileController implements Initializable{
         Thread thread = new Thread(task);
         thread.setDaemon(true);
         thread.start();
+    }
 
+    // helper method
+    private String[] append(String[] arr, String a){
+        String[] temp = new String[arr.length+1];
+        for(int x = 0; x<arr.length; x++){
+            temp[x] = arr[x];
+        }
+        temp[arr.length] = a;
+
+        return temp;
     }
 
     public void change_output_dir_click(){
@@ -105,7 +136,7 @@ public class DecompileController implements Initializable{
         directoryChooser();
     }
 
-    public void directoryChooser() {
+    private void directoryChooser() {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         File selectedDirectory =
                 directoryChooser.showDialog(null);
