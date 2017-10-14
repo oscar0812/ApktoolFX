48efa98418e960d1bba453566941682bea3b94ce
oscar0812 <oscar0812torres@gmail.com>
2017-10-14 01:34:49 -0500

    removed excessive imports

--- src/com/bittle/apktoolfx/MainController.java
+++ src/com/bittle/apktoolfx/MainController.java
@@ -2,18 +2,10 @@ package com.bittle.apktoolfx;
 
 import com.bittle.apktoolfx.compile.CompileController;
 import com.bittle.apktoolfx.decompile.DecompileController;
-import javafx.concurrent.Task;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.input.DragEvent;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.TransferMode;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextFlow;
-import javafx.stage.DirectoryChooser;
-
-import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -25,9 +17,6 @@ public class MainController implements Initializable {
 
     @FXML private Tab decompileTab;
     @FXML private DecompileController decompileController;
-
-    // apk vars
-    String current_apk_path = "";
 
     @Override
     public void initialize(URL url, ResourceBundle bundle) {
